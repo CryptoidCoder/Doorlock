c8980a6a799561a81e206dc0a7155bcffb934d2a
josh leach <45842827+legoman101@users.noreply.github.com>
2020-03-25 13:40:07 +0000

    Update GPIOKILL.py

--- GPIOKILL.py
+++ GPIOKILL.py
@@ -1,1 +1,206 @@
 
+#imports
+import RPi.GPIO as GPIO
+import time
+
+#ledpin variable enter what pin the BUZZER is in below
+pin1 = 1
+pin2 = 1
+
+#GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+#pins
+GPIO.setup(pin1, GPIO.OUT)
+pin1 = pin1+1
+
+
+
+
+
+
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
+#pins
+GPIO.output(pin2, GPIO.LOW)
+pin2 = pin2+1
+
