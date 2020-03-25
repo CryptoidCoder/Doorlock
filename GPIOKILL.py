ad3e548dca5a4d6184b64d89f19ba4db807a4931
josh leach <45842827+legoman101@users.noreply.github.com>
2020-03-25 13:46:00 +0000

    Update GPIOKILL.py

--- GPIOKILL.py
+++ GPIOKILL.py
@@ -5,12 +5,14 @@ import time
 
 #ledpin variable enter what pin the BUZZER is in below
 pin1 = 1
-pin2 = 1
+
 
 #GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+
+print ("KILLING GPIOs...")
 #pins
 GPIO.setup(pin1, GPIO.OUT)
 pin1 = pin1+1
@@ -94,113 +96,86 @@ pin1 = pin1+1
 
 
 
-
-
-#pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
+pin1 = 1
 
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
-
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 #pins
-GPIO.output(pin2, GPIO.LOW)
-pin2 = pin2+1
+GPIO.output(pin1, GPIO.LOW)
+pin1 = pin1+1
 
+
+print("GPIO KILLED")
